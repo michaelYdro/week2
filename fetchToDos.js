4e0a65936712c94b8e8c86082eb72d1cdd82d8ba
michaelYdro <108540844+michaelYdro@users.noreply.github.com>
2022-07-26 17:06:02 -0500

    Create fetchToDos.js
    full fetchTodos from further week included to save time

---
+++ fetchToDos.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+function wait(ms) {
+    return new Promise(function (resolve)
+     {setTimeout(function () 
+        {resolve(ms);}, ms);});}
+
+   const fakeApi = {
+    fetchToDos: async () => {
+        await wait(50);
+        return [
+            { task: 'Make Bed', due: new Date(2009, 5, 5) },
+            { task: 'Clean room', due: new Date(3000, 5, 5) },
+            ];
+        }
+    }
+
+   async function getFutureToDos (api){
+    try {
+        const today = new Date();
+        const todos = await api.fetchToDos();
+            return todos.filter((todo) => {
+                return todo.due < today;
+              })
+            } catch (e){
+                console.log("there was a problem " + error);
+                throw new Error();
+            }
+        }
+
+
+getFutureToDos(fakeApi)
+.then((futureToDos) => {
+    console.log(futureToDos);
+})
